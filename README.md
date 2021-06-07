693bf908496324a42cab7c5844feb49eed0ee8d5
Ola Wethal Petersen <olawp1997@gmail.com>
2021-06-07 02:39:38 +0200

    Initial commit and husky test. Also setup of ESLint, prettier and webpack.

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # home
-A page used for playing around with a variety of different technologies i find interesting
+
+A page used for playing around with a variety of different technologies i find interesting at the time.
+
+## Revision 1
+
+Setting ut some stuff like:
+
+- Webpack
+- Prettier
+- ESLint
+- Typescript
+- Babel
+- Husky and lint-staged
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  mode: "development",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    port: 5000,
+  },
+  module: {
+    // Rules for stylesheets, images and fonts.
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
